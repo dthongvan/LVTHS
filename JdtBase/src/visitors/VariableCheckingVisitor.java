fcd50ae36f3305297e3bd06712720a6dd14af40d
Trantamhvbc <tamtr1998@gmail.com>
2025-05-19 21:07:38 +0700

    Chore: Show all def path by x

--- JdtBase/src/visitors/VariableCheckingVisitor.java
+++ JdtBase/src/visitors/VariableCheckingVisitor.java
@@ -4,39 +4,77 @@ import cfg.generation.CFGGeneration;
 import cfg.generation.CFGGenerationSubCondition;
 import cfg.generation.testpath.Check.FullTestpaths;
 //import cfg.nodes.ICfgNode;
+import cfg.generation.testpath.Check.IFullTestpath;
 import cfg.nodes.DeclarationStatementCfgNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import cfg.nodes.ICfgNode;
-import org.eclipse.jdt.core.dom.VariableDeclaration;
 
 //import java.util.ArrayList;
 //import  visitors.TestPathGeneration;
 
 public class VariableCheckingVisitor extends ASTVisitor {
 
+    private IFullTestpath a;
+
     public boolean visit(MethodDeclaration node) {
         System.out.println(node.parameters());
         System.out.println(node.getAST());
-        System.out.println("method" );
+//        System.out.println("method" );
         CFGGeneration cfgGeneration = new CFGGeneration(node);
-        System.out.println("check cfgGeneration");
-        System.out.println("Get all node:");
+//        System.out.println("check cfgGeneration");
+//        System.out.println("Get all node:");
         ArrayList<ICfgNode> iCfgNodes = cfgGeneration.generateCFG().getAllNodes();
-//        ICFG cfg = cfgGeneration.generateCFG();
-//        CFGGenerationSubCondition cfgGenerationForSubCondition = new CFGGenerationSubCondition(cfg, 1);
-//        ICFG cfgSubCondition = cfgGenerationForSubCondition.generateCFG();
-//        FullTestpaths testPaths = new  TestPathGeneration(cfgSubCondition).getPossibleTestpaths();
+        ICFG cfg = cfgGeneration.generateCFG();
+        CFGGenerationSubCondition cfgGenerationForSubCondition = new CFGGenerationSubCondition(cfg, 1);
+        ICFG cfgSubCondition = cfgGenerationForSubCondition.generateCFG();
+        FullTestpaths testPaths = new  TestPathGeneration(cfgSubCondition).getPossibleTestpaths();
+
         ArrayList<String> variableNames = this.getAllVariables(iCfgNodes);
         Set<String> variableSet = new HashSet<>(variableNames);
         for (String variableName : variableSet) {
-            System.out.println(variableName + " " +this.checkDuplicateDeclarationVariable(variableName,variableNames));
+            ArrayList<IFullTestpath> truePaths = new ArrayList<>();
+            ArrayList<IFullTestpath> falsePaths = new ArrayList<>();
+            for( int i = 0 ; i < testPaths.size(); i++){
+                if(this.checkPathDuplicateDeclarationVariable(testPaths.get(i), variableName=variableName)) truePaths.add(testPaths.get(i));
+                else falsePaths.add(testPaths.get(i));
+            }
+            System.out.println(variableName);
+            System.out.println("TruePaths: ------------->>>>> " );
+
+            for(IFullTestpath truePath : truePaths){
+                System.out.println(truePath.toString());
+            }
+            System.out.println("FalsePaths: ------------->>>>>" );
+
+            for(IFullTestpath falsePath : falsePaths){
+                System.out.println(falsePath.toString());
+            }
+            System.out.println("************************");
+
         }
+        return true;
+    }
+
+    public boolean checkPathDuplicateDeclarationVariable(IFullTestpath path, String variableName) {
+        int counter = 0;
+        for(ICfgNode e: path.getAllCfgNodes()){
+            if (e instanceof DeclarationStatementCfgNode) {
+                if (this.getVariablesNames(e).contains(variableName)) {
+                    counter++;
+                }
+                if (counter > 1) {
+                    return false;
+                }
+            }
+        }
+
         return true;
     }
 
@@ -47,9 +85,7 @@ public class VariableCheckingVisitor extends ASTVisitor {
                 counter++;
             }
         }
-        if(counter == 1){
-            return true;
-        }
+        if(counter == 1) return true;
         return false;
     }
     public ArrayList<String> getAllVariables(ArrayList<ICfgNode> iCfgNodes ) {
@@ -62,6 +98,11 @@ public class VariableCheckingVisitor extends ASTVisitor {
         return variables;
     }
 
+    public ArrayList<List<ICfgNode>> getAllPathsDefOfVariables(ArrayList<ICfgNode> iCfgNodes , String variableName) {
+        ArrayList<List<ICfgNode>> result = new ArrayList<>();
+
+        return result;
+    }
 
     public  ArrayList<String> getVariablesNames(ICfgNode iCfgNode ) {
         String declaration = iCfgNode.getContent(); // ví dụ: "int a, b = 5, c;"
